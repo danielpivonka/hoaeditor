48f4c7a0f5ed9e0041cfbf87d5121a862ee73128
Daniel Pivonka <xpivonk1@fi.muni.cz>
2020-11-20 20:32:13 +0100

    removed json testing

--- tests/parser.test.js
+++ tests/parser.test.js
@@ -20,10 +20,8 @@ test('correct with etc', () => {
     [t] 1 {1}\
     --END--';
     let hoaOut = 'HOA: v1\nStates: 2\nStart: 0\nacc-name: Rabin 1\nAcceptance: 2 (Fin(0)&Inf(1))\nAP: 2 "a" "b"\nadditional: property1 property2\nadditional2: "property1" property2\n--BODY--\nState: 0 "a U b"\n[0&!1] 0 {0}\n[1] 1 {0}\nState: 1\n[t] 1 {1}\n--END--\n';
-    let json = '{"start":[[0]],"positions":[],"aliases":[],"ap":["a","b"],"properties":[],"etc":[["additional:","property1","property2"],["additional2:","\\\"property1\\\"","property2"]],"states":[{"number":0,"accSets":[],"edges":[{"stateConj":[0],"accSets":[0],"label":"0&!1"},{"stateConj":[1],"accSets":[0],"label":"1"}],"name":"a U b"},{"number":1,"accSets":[],"edges":[{"stateConj":[1],"accSets":[1],"label":"t"}]}],"version":"v1","stateCount":2,"accname":"Rabin 1","acceptance":{"count":2,"str":"(Fin(0)&Inf(1))"}}';
     let result = parse(hoaIn);
     expect(result.toHoaString()).toBe(hoaOut);
-    expect(result).toEqual(JSON.parse(json));
 })
 
 test('correct single line transitions with accepting states', () => {
@@ -45,10 +43,8 @@ test('correct single line transitions with accepting states', () => {
     2 2 2 2\
     --END--';
     let hoaOut = 'HOA: v1\nStates: 3\nStart: 0\nacc-name: Rabin 1\nAcceptance: 2 (Fin(0)&Inf(1))\nAP: 2 "a" "b"\n--BODY--\nState: 0 "a U b" {0}\n2\n0\n1\n1\nState: 1 {1}\n1\n1\n1\n1\nState: 2 "sink state" {0}\n2\n2\n2\n2\n--END--\n';
-    let json = '{"start":[[0]],"positions":[],"aliases":[],"ap":["a","b"],"properties":[],"etc":[],"states":[{"number":0,"accSets":[0],"edges":[{"stateConj":[2],"accSets":[]},{"stateConj":[0],"accSets":[]},{"stateConj":[1],"accSets":[]},{"stateConj":[1],"accSets":[]}],"name":"a U b"},{"number":1,"accSets":[1],"edges":[{"stateConj":[1],"accSets":[]},{"stateConj":[1],"accSets":[]},{"stateConj":[1],"accSets":[]},{"stateConj":[1],"accSets":[]}]},{"number":2,"accSets":[0],"edges":[{"stateConj":[2],"accSets":[]},{"stateConj":[2],"accSets":[]},{"stateConj":[2],"accSets":[]},{"stateConj":[2],"accSets":[]}],"name":"sink state"}],"version":"v1","stateCount":3,"accname":"Rabin 1","acceptance":{"count":2,"str":"(Fin(0)&Inf(1))"}}';
     let result = parse(hoaIn);
     expect(result.toHoaString()).toBe(hoaOut);
-    expect(result).toEqual(JSON.parse(json));
 })
 
 test('correct with name and tool', () => {
@@ -68,10 +64,8 @@ test('correct with name and tool', () => {
       0 {0 1} /*  a  &  b */\
     --END--';
     let hoaOut = 'HOA: v1\nStates: 1\nStart: 0\nacc-name: generalized-Buchi 2\nAcceptance: 2 (Inf(0)&Inf(1))\nAP: 2 "a" "b"\ntool: "tooly"\nname: "GFa & GFb"\n--BODY--\nState: 0\n0\n0 {0}\n0 {1}\n0 {0 1}\n--END--\n';
-    let json = '{"start":[[0]],"positions":[],"aliases":[],"tool":"tooly","ap":["a","b"],"properties":[],"etc":[],"states":[{"number":0,"accSets":[],"edges":[{"stateConj":[0],"accSets":[]},{"stateConj":[0],"accSets":[0]},{"stateConj":[0],"accSets":[1]},{"stateConj":[0],"accSets":[0,1]}]}],"version":"v1","name":"GFa & GFb","stateCount":1,"accname":"generalized-Buchi 2","acceptance":{"count":2,"str":"(Inf(0)&Inf(1))"}}';
     let result = parse(hoaIn);
     expect(result.toHoaString()).toBe(hoaOut);
-    expect(result).toEqual(JSON.parse(json));
 })
 
 test('correct with labeled', () => {
@@ -90,10 +84,8 @@ test('correct with labeled', () => {
       [0 & 1]   0 {0 1}\
     --END--';
     let hoaOut = 'HOA: v1\nStates: 1\nStart: 0\nacc-name: generalized-Buchi 2\nAcceptance: 2 (Inf(0)&Inf(1))\nAP: 2 "a" "b"\nname: "GFa & GFb"\n--BODY--\nState: 0\n[!0&!1] 0\n[0&!1] 0 {0}\n[!0&1] 0 {1}\n[0&1] 0 {0 1}\n--END--\n';
-    let json = '{"start":[[0]],"positions":[],"aliases":[],"ap":["a","b"],"properties":[],"etc":[],"states":[{"number":0,"accSets":[],"edges":[{"stateConj":[0],"accSets":[],"label":"!0&!1"},{"stateConj":[0],"accSets":[0],"label":"0&!1"},{"stateConj":[0],"accSets":[1],"label":"!0&1"},{"stateConj":[0],"accSets":[0,1],"label":"0&1"}]}],"version":"v1","name":"GFa & GFb","stateCount":1,"accname":"generalized-Buchi 2","acceptance":{"count":2,"str":"(Inf(0)&Inf(1))"}}';
     let result = parse(hoaIn);
     expect(result.toHoaString()).toBe(hoaOut);
-    expect(result).toEqual(JSON.parse(json));
 })
 test('correct with aliases', () => {
     let hoaIn = 'HOA: v1\
@@ -113,10 +105,8 @@ test('correct with aliases', () => {
     [@a & @bc]   0 {0 1}\
     --END--';
     let hoaOut = 'HOA: v1\nStates: 1\nStart: 0\nacc-name: generalized-Buchi 2\nAcceptance: 2 (Inf(0)&Inf(1))\nAP: 3 "a" "b" "c"\nname: "GFa & GF(b & c)"\nAlias: @a 0\nAlias: @bc 1&2\n--BODY--\nState: 0\n[!@a&!@bc] 0\n[@a&!@bc] 0 {0}\n[!@a&@bc] 0 {1}\n[@a&@bc] 0 {0 1}\n--END--\n';
-    let json = '{"start":[[0]],"positions":[],"aliases":[{"name":"@a","expr":"0"},{"name":"@bc","expr":"1&2"}],"ap":["a","b","c"],"properties":[],"etc":[],"states":[{"number":0,"accSets":[],"edges":[{"stateConj":[0],"accSets":[],"label":"!@a&!@bc"},{"stateConj":[0],"accSets":[0],"label":"@a&!@bc"},{"stateConj":[0],"accSets":[1],"label":"!@a&@bc"},{"stateConj":[0],"accSets":[0,1],"label":"@a&@bc"}]}],"version":"v1","name":"GFa & GF(b & c)","stateCount":1,"accname":"generalized-Buchi 2","acceptance":{"count":2,"str":"(Inf(0)&Inf(1))"}}';
     let result = parse(hoaIn);
     expect(result.toHoaString()).toBe(hoaOut);
-    expect(result).toEqual(JSON.parse(json));
 })
 test('correct with state labels', () => {
     let hoaIn = 'HOA: v1\
@@ -134,10 +124,8 @@ test('correct with state labels', () => {
     0 1\
     --END--';
     let hoaOut = 'HOA: v1\nStates: 2\nStart: 0\nStart: 1\nacc-name: Buchi\nAcceptance: 1 Inf(0)\nAP: 1 "a"\nname: "GFa"\n--BODY--\nState: [0] 0 {0}\n0\n1\nState: [!0] 1\n0\n1\n--END--\n';
-    let json = '{"start":[[0],[1]],"positions":[],"aliases":[],"ap":["a"],"properties":[],"etc":[],"states":[{"number":0,"accSets":[0],"edges":[{"stateConj":[0],"accSets":[]},{"stateConj":[1],"accSets":[]}],"label":"0"},{"number":1,"accSets":[],"edges":[{"stateConj":[0],"accSets":[]},{"stateConj":[1],"accSets":[]}],"label":"!0"}],"version":"v1","name":"GFa","stateCount":2,"accname":"Buchi","acceptance":{"count":1,"str":"Inf(0)"}}';
     let result = parse(hoaIn);
     expect(result.toHoaString()).toBe(hoaOut);
-    expect(result).toEqual(JSON.parse(json));
 })
 test('correct with properties', () => {
     let hoaIn = 'HOA: v1\
@@ -164,8 +152,6 @@ test('correct with properties', () => {
     [!0&!1] 3\
     --END--';
     let hoaOut = 'HOA: v1\nStates: 4\nStart: 0\nacc-name: Buchi\nAcceptance: 1 Inf(0)\nAP: 2 "a" "b"\nname: "GFa | G(b <-> Xa)"\nproperties: explicit-labels trans-labels\n--BODY--\nState: 0\n[t] 1\n[1] 2\n[!1] 3\nState: 1 "GFa"\n[0] 1 {0}\n[!0] 1\nState: 2 "a & G(b <-> Xa)" {0}\n[0&1] 2\n[0&!1] 3\nState: 3 "!a & G(b <-> Xa)" {0}\n[!0&1] 2\n[!0&!1] 3\n--END--\n';
-    let json = '{"start":[[0]],"positions":[],"aliases":[],"ap":["a","b"],"properties":["explicit-labels","trans-labels"],"etc":[],"states":[{"number":0,"accSets":[],"edges":[{"stateConj":[1],"accSets":[],"label":"t"},{"stateConj":[2],"accSets":[],"label":"1"},{"stateConj":[3],"accSets":[],"label":"!1"}]},{"number":1,"accSets":[],"edges":[{"stateConj":[1],"accSets":[0],"label":"0"},{"stateConj":[1],"accSets":[],"label":"!0"}],"name":"GFa"},{"number":2,"accSets":[0],"edges":[{"stateConj":[2],"accSets":[],"label":"0&1"},{"stateConj":[3],"accSets":[],"label":"0&!1"}],"name":"a & G(b <-> Xa)"},{"number":3,"accSets":[0],"edges":[{"stateConj":[2],"accSets":[],"label":"!0&1"},{"stateConj":[3],"accSets":[],"label":"!0&!1"}],"name":"!a & G(b <-> Xa)"}],"version":"v1","name":"GFa | G(b <-> Xa)","stateCount":4,"accname":"Buchi","acceptance":{"count":1,"str":"Inf(0)"}}';
     let result = parse(hoaIn);
     expect(result.toHoaString()).toBe(hoaOut);
-    expect(result).toEqual(JSON.parse(json));
 })
